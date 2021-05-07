bac6398f6afbe760dd6902318008cdf57abdc2e9
hope5107@naver.com <hope5107@naver.com>
2021-05-07 19:36:50 +0900

    slick slider 적용

--- src/menus/menu.js
+++ src/menus/menu.js
@@ -3,8 +3,10 @@ import WeeklyRank from '../components/weeklyRank';
 import MovieDetail from '../components/movieDetail';
 import ActorList from '../components/actorSearch';
 import ActorDetail from '../components/actorDetail';
+import Main from '../components/main';
 
 export const menuList = [ 
+    {key: 'main', txt: '메인', cont: '메인', url: '/', component: Main, gnb: false},
     {key: 'dailyRank', txt: '일간박스오피스', cont: '일간 박스오피스를 조회해보세요.', url: '/dailyRank', component: DailyRank, gnb: true},
     {key: 'weeklyRank', txt: '주간박스오피스', cont: '주간 박스오피스를 조회해보세요.', url: '/weeklyRank', component: WeeklyRank, gnb: true},
     {key: 'movieDetail', txt: '영화상세정보', cont: '영화상세정보를 확인하세요.', url: '/movieDetail/:movieCd/', component: MovieDetail, gnb: false},
