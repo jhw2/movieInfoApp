2b3bcb5155c2633d7c4c9df5401a903ea030f6ec
hope5107@naver.com <hope5107@naver.com>
2021-10-22 10:31:43 +0900

    og태그 추가

--- src/http/http-common-naver.js
+++ src/http/http-common-naver.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://cors-jin.herokuapp.com/https://openapi.naver.com/v1/search",
+  //baseURL: "http://18.220.253.22:8080/https://openapi.naver.com/v1/search",
   headers: {
     "Content-type": "application/json",
     'X-Naver-Client-Id': "QA8UjSKONNKwkAUuOXtp",
