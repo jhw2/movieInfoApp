e00b6bd6d0c2a39a2b17c10152673b6c18e9ca7a
hope5107@naver.com <hope5107@naver.com>
2021-11-01 11:15:24 +0900

    모바일 뒤로가기 버튼 추가

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,9 @@
 import { memo, useState, useCallback } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import RightMenu from './RightMenu';
 
 const Header = memo(({menuList})=>{
-    
+    const history = useHistory();
     const [mobileMenuOn, setMobileMenuOn] = useState(false);
     const menuClose = useCallback(()=>{
         setMobileMenuOn(false);
@@ -12,12 +12,17 @@ const Header = memo(({menuList})=>{
         setMobileMenuOn(true);
     }, [])
 
+    const pagePrev = useCallback(()=>{
+        history.goBack();
+    }, [history])
+
     return (
         <header className={mobileMenuOn ? 'mv-header nav-open' : 'mv-header'}> 
             <div className='m-menu-bg' onClick={menuClose}></div>
 
             <div className='group'>
                 <button className='ic-menu' onClick={menuOpen}>모바일메뉴</button>
+                <button className='ic-prev' onClick={pagePrev}>뒤로가기</button>
                 <div className="hd-logo"><a href="/">Movie<strong>Box</strong></a></div>
                 <ul className="gnb">
                     {menuList.map((menu, i)=>{
