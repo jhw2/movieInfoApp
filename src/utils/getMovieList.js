e2f6ab92201c581207156373e8d99788d3ad8327
hope5107@naver.com <hope5107@naver.com>
2021-11-02 13:45:55 +0900

    영화 포스터 캐시 저장

--- src/utils/getMovieList.js
+++ src/utils/getMovieList.js
@@ -1,21 +1,35 @@
 import MovieSearchService from '../http/movieSearchService';
+const moviePosters = localStorage.moviePosters ? JSON.parse(localStorage.moviePosters) : {};
 
 export const getPosterData = async (dataList)=>{
     const dataListHasPoster = [...dataList];
     const promiseList = dataList.map( async (val, i)=>{
         const { movieNm } = val;
-        return MovieSearchService.getSearchPoster(movieNm);
+        const posterData = moviePosters[movieNm] ? moviePosters[movieNm] : MovieSearchService.getSearchPoster(movieNm)
+        return posterData;
     });
 
     const posters = await Promise.allSettled(promiseList)
-    posters.forEach(({ value }, i)=>{
+    posters.forEach(({value}, i)=>{
         let poster = 'no-data';
-        const data = value.data;
-        if(data.items.length !== 0){
-            poster = data.items[0].image;
+        
+        //cache data
+        if(typeof value === 'string'){
+            poster = value;
+        }
+        //api call data
+        if(typeof value === 'object'){
+            const data = value.data;
+            if(data.items.length !== 0){
+                poster = data.items[0].image;
+                const movieNm = data.items[0].title.replace(/(<([^>]+)>)/ig,"");
+                moviePosters[movieNm] = poster;
+            }
         }
         dataListHasPoster[i].poster = poster;
     });
+
+    localStorage.moviePosters = JSON.stringify(moviePosters);
 
     return dataListHasPoster;
 }
